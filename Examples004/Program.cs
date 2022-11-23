f824f2e4eaaa09b6ca614aeb8bf2a05aee85ff32
Peter <renemebel@yandex.ru>
2022-11-23 19:08:57 +0300

    случайные числаm

---
+++ Examples004/Program.cs
@@ -0,0 +1,7 @@
+﻿// случайные числа
+int NumA = new Random().Next(1,10);
+Console.WriteLine(NumA);
+int NumB = new Random().Next(1,10);
+Console.WriteLine(NumB);
+int result = NumA + NumB;
+Console.WriteLine(result);
